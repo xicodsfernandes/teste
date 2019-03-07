3589e89aa1a6e2e4efe9dafc882c7f57c2b1a107
xicodsfernandes <47608138+xicodsfernandes@users.noreply.github.com>
2019-03-07 16:09:55 +0000

    Add files via upload
    estrutura a seguir

---
+++ estrutura.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct divisao {
+   char  nome[50];
+   char  sensores[10][30];
+   int sensores_estado[10];
+   char  atuadores[10][30];
+   int  atuadores_estado[10];
+};
+
+int main(){
+
+struct divisao casa[5];
+
+}
